783f22022d9c5c549efc9368a08b8ca95e441c90
erkinov-khumoyun <erkinovkhumoyun@gmail.com>
2022-12-13 23:14:15 +0500

    .

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,4 +1,38 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm): #Biz hozir user moedli uchum forma yaratdik
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username','first_name','email']
+
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','email']
+
+class ProfileEditForm(forms.ModelForm): #Bu bizning profile malumotlarimza Profile modelga saqlaydi,va USer tug`ilgan kunini yoza oldai va upload qila oldi rasmi
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth','photo']
+
+
